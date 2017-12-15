1ca0053182aae6b1d6c9d2cbb0dcf4a81a81fb96
Matt Jackson <matjack9@gmail.com>
2017-12-15 19:47:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  result = nil
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      result = contestant["name"].split(" ")[0]
+    end
+  end
+  result
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  result = nil
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        result = contestant["name"]
+      end
+    end
+  end
+  result
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
+
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  count = 0
+  data[season].each do |contestant|
+    sum += contestant["age"].to_f
+    count += 1
+  end
+  (sum/count).round
 end
